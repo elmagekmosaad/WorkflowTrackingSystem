eb787bc366dd95505cde0f06a7664344ceaae7bf
elmagekmosaad <elmagekmosaad3535@gmail.com>
2025-09-13 14:56:43 +0300

    Add IWorkflowRepository interface for workflow checks
    Introduces the IWorkflowRepository interface in the
    WorkflowTrackingSystem.Domain.Repositories namespace.
    This interface extends IBaseRepository<Workflow> and
    includes the ExistsAsync method to verify the existence
    of a workflow by its ID.

---
+++ WorkflowTrackingSystem.Domain/Repositories/IWorkflowRepository.cs
@@ -0,0 +1,9 @@
+﻿using WorkflowTrackingSystem.Domain.Entities;
+
+namespace WorkflowTrackingSystem.Domain.Repositories
+{
+    public interface IWorkflowRepository : IBaseRepository<Workflow>
+    {
+        Task<bool> ExistsAsync(Guid id);
+    }
+}
