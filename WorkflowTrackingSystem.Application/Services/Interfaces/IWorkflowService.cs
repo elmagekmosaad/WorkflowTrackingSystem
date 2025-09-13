460d2cca511561b1474d3154c987c94d0e97a5ef
elmagekmosaad <elmagekmosaad3535@gmail.com>
2025-09-13 14:52:50 +0300

    Add IWorkflowService interface for workflow management
    Introduces the `IWorkflowService` interface in the
    `WorkflowTrackingSystem.Application.Services.Interfaces` namespace.
    This interface includes five asynchronous methods for managing
    workflows: `GetAllWorkflowsAsync`, `GetWorkflowByIdAsync`,
    `CreateWorkflowAsync`, `UpdateWorkflowAsync`, and
    `DeleteWorkflowAsync`. Additional using directives for
    necessary namespaces have also been added.

---
+++ WorkflowTrackingSystem.Application/Services/Interfaces/IWorkflowService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkflowTrackingSystem.Application.DTOs;
+using WorkflowTrackingSystem.Domain.Entities;
+
+namespace WorkflowTrackingSystem.Application.Services.Interfaces
+{
+    public interface IWorkflowService
+    {
+        Task<IEnumerable<Workflow>> GetAllWorkflowsAsync();
+        Task<Workflow?> GetWorkflowByIdAsync(Guid id);
+        Task<Workflow> CreateWorkflowAsync(CreateWorkflowDto dto);
+        Task<Workflow> UpdateWorkflowAsync(Guid id, CreateWorkflowDto dto);
+        Task DeleteWorkflowAsync(Guid id);
+    }
+}
