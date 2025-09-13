6c2f2396010bd7a082a9030709cf16469135ef40
elmagekmosaad <elmagekmosaad3535@gmail.com>
2025-09-13 15:17:35 +0300

    Add WorkflowRepository with ExistsAsync method
    Introduces the WorkflowRepository class implementing the
    IWorkflowRepository interface. Inherits from BaseRepository<Workflow>
    and includes a constructor that accepts an AppDbContext.
    Adds an asynchronous method ExistsAsync to check for the
    existence of a workflow by its Guid using Entity Framework Core.

---
+++ WorkflowTrackingSystem.Infrastructure/Repositories/WorkflowRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkflowTrackingSystem.Domain.Entities;
+using WorkflowTrackingSystem.Domain.Repositories;
+using WorkflowTrackingSystem.Infrastructure.Data;
+
+namespace WorkflowTrackingSystem.Infrastructure.Repositories
+{
+    public class WorkflowRepository : BaseRepository<Workflow>, IWorkflowRepository
+    {
+        public WorkflowRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<bool> ExistsAsync(Guid id)
+        {
+            return await _context.Workflows.AnyAsync(w => w.Id == id);
+        }
+    }
+}
