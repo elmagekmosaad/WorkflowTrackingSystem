e41bc71a84d756d40ce0ee99bf765bc22ed10659
elmagekmosaad <elmagekmosaad3535@gmail.com>
2025-09-13 15:16:57 +0300

    Add AppDbContext for managing workflows and steps
    Introduces a new `AppDbContext` class inheriting from `DbContext` to handle the database context.
    Includes `DbSet` properties for `Workflows` and `WorkflowSteps`.
    Overrides `OnModelCreating` to configure entity relationships, set primary keys, and establish a foreign key relationship with cascading delete behavior.

---
+++ WorkflowTrackingSystem.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorkflowTrackingSystem.Domain.Entities;
+
+namespace WorkflowTrackingSystem.Infrastructure.Data
+{
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Workflow> Workflows { get; set; }
+        public DbSet<WorkflowStep> WorkflowSteps { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            #region Workflow configuration
+            modelBuilder.Entity<Workflow>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.HasIndex(e => e.Name);
+            });
+            #endregion
+
+            #region WorkflowStep configuration
+            modelBuilder.Entity<WorkflowStep>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.HasOne(e => e.Workflow)
+                      .WithMany(w => w.Steps)
+                      .HasForeignKey(e => e.WorkflowId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+            #endregion
+
+
+        }
+    }
+}
