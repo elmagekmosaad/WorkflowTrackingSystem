6d4e494e7dcc9b47bef550fa9fd52d6cc8687975
elmagekmosaad <elmagekmosaad3535@gmail.com>
2025-09-13 15:06:59 +0300

    Add WorkflowsController for API versioning
    Introduces a new `WorkflowsController` class in the
    `WorkflowTrackingSystem.Api.Controllers.v1` namespace.
    Includes using directives for `Asp.Versioning` and
    `Microsoft.AspNetCore.Mvc`. The class is set up with
    routing and API versioning attributes for version "1.0",
    but currently contains no methods or properties.

---
+++ WorkflowTrackingSystem.Api/Controllers/v1/WorkflowsController.cs
@@ -0,0 +1,12 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WorkflowTrackingSystem.Api.Controllers.v1
+{
+    [Route("v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class WorkflowsController : ControllerBase
+    {
+    }
+}
