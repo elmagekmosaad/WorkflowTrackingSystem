079cf8b17c24b4d9311e5f882b8ce6a2fdbb03d3
elmagekmosaad <elmagekmosaad3535@gmail.com>
2025-09-13 15:17:14 +0300

    Add BaseRepository class for CRUD operations
    Introduces a generic `BaseRepository<T>` class implementing the `IBaseRepository<T>` interface. This class provides essential CRUD methods such as `AddAsync`, `Update`, `Delete`, `GetAllAsync`, `GetByIdAsync`, `FindAsync`, and `FindAllAsync`, leveraging Entity Framework Core for database interactions. It supports asynchronous operations and allows for including related entities in queries.

---
+++ WorkflowTrackingSystem.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using WorkflowTrackingSystem.Domain.Entities;
+using WorkflowTrackingSystem.Domain.Repositories;
+using WorkflowTrackingSystem.Infrastructure.Data;
+
+namespace WorkflowTrackingSystem.Infrastructure.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        protected AppDbContext _context;
+
+        public BaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public T Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            _context.SaveChanges();
+            return entity;
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+      
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<T> FindAsync(Expression<Func<T, bool>> criteria, string[] includes = null)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            if (includes != null)
+                foreach (var incluse in includes)
+                    query = query.Include(incluse);
+
+            return await query.SingleOrDefaultAsync(criteria);
+        }
+
+        public async Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> criteria, string[] includes = null)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            if (includes != null)
+                foreach (var include in includes)
+                    query = query.Include(include);
+
+            return await query.Where(criteria).ToListAsync();
+        }
+    }
+}
