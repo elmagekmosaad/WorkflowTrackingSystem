2b110568b4e3899434fa54f2aa4dfdda304d3b85
elmagekmosaad <elmagekmosaad3535@gmail.com>
2025-09-13 23:32:22 +0300

    Add AutoMapper profiles for Workflow and WorkflowStep
    Introduce `WorkflowProfile` and `WorkflowStepProfile` to define mappings between domain entities and their DTOs. Include mappings for creating workflows and workflow steps from request objects, utilizing `ReverseMap()` for bi-directional conversions.

---
+++ WorkflowTrackingSystem.Api/Mapping/WorkflowStepProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using WorkflowTrackingSystem.Api.Requests;
+using WorkflowTrackingSystem.Application.DTOs;
+using WorkflowTrackingSystem.Domain.Entities;
+
+namespace WorkflowTrackingSystem.Api.Mapping
+{
+    public class WorkflowStepProfile : Profile
+    {
+        public WorkflowStepProfile()
+        {
+            CreateMap<WorkflowStep, WorkflowStepDto>()
+                .ReverseMap();
+
+            CreateMap<CreateWorkflowStepRequest, WorkflowStepDto>()
+                .ReverseMap();
+
+            CreateMap<WorkflowStep, WorkflowStepDto>()
+                .ReverseMap();
+
+           
+        }
+    }
+
+   
+}
