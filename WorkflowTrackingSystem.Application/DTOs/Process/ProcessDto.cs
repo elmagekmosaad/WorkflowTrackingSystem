44764a12cdfe3798493cd11f7368e1339b133fb1
elmagekmosaad <elmagekmosaad3535@gmail.com>
2025-09-14 10:29:28 +0300

    Add ProcessDto class in WorkflowTrackingSystem namespace
    Introduced a new namespace `WorkflowTrackingSystem.Application.DTOs.Process`
    and added the `ProcessDto` class. The class includes three properties:
    `Id` (Guid), `WorkflowId` (Guid), and `Initiator` (string, initialized to an empty string).

---
+++ WorkflowTrackingSystem.Application/DTOs/Process/ProcessDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WorkflowTrackingSystem.Application.DTOs.Process
+{
+    public class ProcessDto
+    {
+        public Guid Id { get; set; }
+        public Guid WorkflowId { get; set; }
+        public string Initiator { get; set; } = string.Empty;
+    }
+}
