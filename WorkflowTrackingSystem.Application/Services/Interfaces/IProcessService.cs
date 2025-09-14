c76c8558056965b5c57b430deaa24684e39d77c0
elmagekmosaad <elmagekmosaad3535@gmail.com>
2025-09-14 10:28:50 +0300

    Refactor IProcessService interface methods
    Added `GetAllProcesssAsync` for paginated process retrieval. Removed the previous declaration of this method and eliminated several commented-out process management methods (Create, Update, Delete) for clarity and maintenance.

--- WorkflowTrackingSystem.Application/Services/Interfaces/IProcessService.cs
+++ WorkflowTrackingSystem.Application/Services/Interfaces/IProcessService.cs
@@ -6,13 +6,9 @@ namespace ProcessTrackingSystem.Application.Services.Interfaces
 {
     public interface IProcessService
     {
+        Task<BaseResponse<PaginatedList<ProcessDto>>> GetAllProcesssAsync(int pageNumber, int pageSize, Guid? workflowId = null, ProcessStatus? status = null, string? assignedTo = null);
         Task<BaseResponse<ProcessDto>> StartProcessAsync(ProcessDto processDto);
         Task<BaseResponse<ProcessDto>> ExecuteStepAsync(ProcessDto processDto);
         Task<BaseResponse<ProcessDto>> GetProcessByIdAsync(Guid id);
-
-        Task<BaseResponse<PaginatedList<ProcessDto>>> GetAllProcesssAsync(int pageNumber, int pageSize, Guid? workflowId = null, ProcessStatus? status = null, string? assignedTo = null);
-        //Task<BaseResponse<ProcessDto>> CreateProcessAsync(ProcessDto workflowDto);
-        //Task<BaseResponse<ProcessDto>> UpdateProcessAsync(Guid id, ProcessDto workflowDto);
-        //Task<BaseResponse<string>> DeleteProcessAsync(Guid id);
     }
 }
