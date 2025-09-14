69291fe65898a30ccacc861199a7dfe74b8535b6
elmagekmosaad <elmagekmosaad3535@gmail.com>
2025-09-14 11:57:57 +0300

    Add ProcessRepository with pagination support
    Introduces the `ProcessRepository` class implementing the `IProcessRepository` interface. This class inherits from `BaseRepository<Process>` and includes a constructor that accepts an `AppDbContext`. Adds the `GetPagedAsync` method to retrieve a paginated list of `Process` entities, with optional filtering by `workflowId`, `status`, and `assignedTo`, while including related `ProcessSteps`.

---
+++ WorkflowTrackingSystem.Infrastructure/Repositories/ProcessRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorkflowTrackingSystem.Domain.Entities;
+using WorkflowTrackingSystem.Domain.Enums;
+using WorkflowTrackingSystem.Domain.Repositories;
+using WorkflowTrackingSystem.Infrastructure.Contexts;
+using WorkflowTrackingSystem.Infrastructure.Repositories;
+using WorkflowTrackingSystem.Shared;
+
+namespace ProcessTrackingSystem.Infrastructure.Repositories
+{
+    public class ProcessRepository : BaseRepository<Process>, IProcessRepository
+    {
+        public ProcessRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<PaginatedList<Process>> GetPagedAsync(int pageNumber, int pageSize, Guid? workflowId = null, ProcessStatus? status = null, string? assignedTo = null)
+        {
+            var query = _context.Processes.Include(w => w.ProcessSteps).AsQueryable();
+            var totalCount = await query.CountAsync();
+            var items = await query
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+            return new PaginatedList<Process>(items, totalCount, pageNumber, pageSize);
+        }
+    }
+}
