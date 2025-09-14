83b06d61c7476293062d64037f676b5dd9d60d57
elmagekmosaad <elmagekmosaad3535@gmail.com>
2025-09-14 10:27:38 +0300

    Add IProcessRepository interface with pagination method
    This commit introduces the `IProcessRepository` interface in the `IProcessRepository.cs` file. It includes several using directives for necessary namespaces and defines a method `GetPagedAsync` to retrieve a paginated list of `Process` objects, allowing for optional filtering by `workflowId`, `status`, and `assignedTo`.

---
+++ WorkflowTrackingSystem.Domain/Repositories/IProcessRepository.cs
@@ -0,0 +1,11 @@
+﻿using WorkflowTrackingSystem.Domain.Entities;
+using WorkflowTrackingSystem.Domain.Enums;
+using WorkflowTrackingSystem.Shared;
+
+namespace WorkflowTrackingSystem.Domain.Repositories
+{
+    public interface IProcessRepository : IBaseRepository<Process>
+    {
+        Task<PaginatedList<Process>> GetPagedAsync(int pageNumber, int pageSize, Guid? workflowId = null, ProcessStatus? status = null, string? assignedTo = null);
+    }
+}
